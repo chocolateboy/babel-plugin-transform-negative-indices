9fd1e05b4c92a04c6b5637f00e65c1dfd93d9084
chocolateboy <chocolate@cpan.org>
2020-05-04 15:08:03 +0100

    test tweaks
    improve the "leave these unchanged" test cases

--- test/fixtures/basic.js
+++ test/fixtures/basic.js
@@ -7,5 +7,7 @@ array[-2] = 42
 // these should be unchanged
 
 let bar = array[0]
-let baz = array[42]
-let quux = array[10 - 0]
+let baz = array[1]
+let quux = array[42]
+
+array[+42] = array[10 -1]
